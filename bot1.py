494068df3c0802ab73ef1456e8053f658694fa3c
wildfireone <wildfireone@gmail.com>
2015-11-05 20:58:07 +0000

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,9 +1,9 @@
 import tweepy
-consumer_key = 'WPukxs264EgldelArs33LLpSd'
-consumer_secret = 's7bw8nrlutE07hVIU
-access_token
-access_token_secret
-auth = tweepy.)AuthHandler(consumer_key, consumer_secret)
+consumer_key = 'WPukxs264EgIdelArs33LLpSd'
+consumer_secret = 's7bw8nrIutE07hVIU2oZNerFsaA6Qmj7VznecpB5B72eOWXdYv'
+access_token = '14812487-x5hEARyRyVE7X4QQ2X1YhnJbb4bICpRjQgNxNIs8J'
+access_token_secret = 'Dozik4wfubV4j8W5zaj8axtsCG4cSzbjm31VveFxqwYfw'
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 twitter = tweepy.API(auth)
 twitter.update("hello world")
